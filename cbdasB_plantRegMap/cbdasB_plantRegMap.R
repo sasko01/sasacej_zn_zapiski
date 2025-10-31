ee13b3ed356fb8cf35c53497650d1503917ebd83
sasa <scej49@gmail.com>
2025-10-31 15:42:18 +0100

    heatmaps - promotrs only

--- cbdasB_plantRegMap/cbdasB_plantRegMap.R
+++ cbdasB_plantRegMap/cbdasB_plantRegMap.R
@@ -16,46 +16,56 @@ minus_strand <- function(x) {
 
 #Plus strand---------------------------------------------------------------------------------------------------
 
-abacus_cbdasPromB_prm <- plus_strand('abacus_cbadsPromB_plantRegMap.txt')
+abacus <- plus_strand('abacus_cbadsPromB_plantRegMap.txt')
 
-finola_cbdasPromB_rpm.txt <- plus_strand('finola_cbdasPromB_rpm.txt')
+finola <- plus_strand('finola_cbdasPromB_rpm.txt')
 
-JL5_cbdasPromB_prm <- plus_strand('JL5_cbdasPromB_prm.txt')
+JL5 <- plus_strand('JL5_cbdasPromB_prm.txt')
 
-JLdash_cbdasPromB_prm <- plus_strand('JLdash_cbdasPromB_prm.txt')
+JLdash <- plus_strand('JLdash_cbdasPromB_prm.txt')
 
-Pbanana_cbdasPromB_prm <- plus_strand('Pbanana_cbdasPromB_prm.txt')
+Pbanana <- plus_strand('Pbanana_cbdasPromB_prm.txt')
 
-PK_cbdasPromB_prm <- plus_strand('PK_cbdasPromB_prm.txt')
+PKush <- plus_strand('PK_cbdasPromB_prm.txt')
 
-SL2_cbdasPromB_prm <- plus_strand('SL2_cbdasPromB_prm.txt')
+SL2 <- plus_strand('SL2_cbdasPromB_prm.txt')
 
 #Minus strand--------------------------------------------------------------------------------------------------
 
-anc1_cbdasPromB_prm <- minus_strand('anc1_cbdasPromB_prm.txt')
+anc1 <- minus_strand('anc1_cbdasPromB_prm.txt')
 
-anc2_cbdasPromB_prm <- minus_strand('anc2_cbdasPromB_prm.txt')
+anc2 <- minus_strand('anc2_cbdasPromB_prm.txt')
 
-cannatonic_cbdasPromB_prm <- minus_strand('cannatonic_cbdasPromB_prm.txt')
+cannatonic <- minus_strand('cannatonic_cbdasPromB_prm.txt')
 
-cannbio_cbdasPromB_prm <- minus_strand('cannbio_cbdasPromB_prm.txt')
+cannbio <- minus_strand('cannbio_cbdasPromB_prm.txt')
 
-CP_cbdasPromB_rpm <- minus_strand('CP_cbdasPromB_rpm.txt')
+CP <- minus_strand('CP_cbdasPromB_rpm.txt')
 
-hOG1_cbdasPromB_prm <- minus_strand('hOG1_cbdasPromB_prm.txt') 
+hOG1 <- minus_strand('hOG1_cbdasPromB_prm.txt') 
 
-hOG2_cbdasPromB_prm <- minus_strand('hOG2_cbdasPromB_rpm.txt')
+hOG2 <- minus_strand('hOG2_cbdasPromB_rpm.txt')
 
-JL_cbdasPromB_prm.txt <- minus_strand('JL_cbdasPromB_prm.txt') 
+JL <- minus_strand('JL_cbdasPromB_prm.txt') 
 
-JLfather_cbdasPromB_prm <- minus_strand('JLfather_cbdasPromB_prm.txt')
+JLfather <- minus_strand('JLfather_cbdasPromB_prm.txt')
 
-JLmother_cbdasPromB_prm <- minus_strand('JLmother_cbdasPromB_prm.txt')
+JLmother <- minus_strand('JLmother_cbdasPromB_prm.txt')
 
-PR_cbdasPromB_prm <- minus_strand('PR_cbdasPromB_prm.txt')
+PR <- minus_strand('PR_cbdasPromB_prm.txt')
 
-SL1_cbdasPromB_prm <- minus_strand('SL1_cbdasPromB_prm.txt')
+SL1 <- minus_strand('SL1_cbdasPromB_prm.txt')
 
-travisCBD1_cbdasPromB_prm <- minus_strand('travisCBD1_cbdasPromB_prm.txt')
+travisCBD1 <- minus_strand('travisCBD1_cbdasPromB_prm.txt')
 
-travisCBD2_cbdasPromB_prm <- minus_strand('travisCBD2_cbdasPromB_prm.txt')
+travisCBD2 <- minus_strand('travisCBD2_cbdasPromB_prm.txt')
+
+Ppepper <- minus_strand('Ppepper_cbdasPromB_prm.txt')
+
+
+#PLOT----------------------------------------------------------------------------------------------------------
+
+
+
+
+
